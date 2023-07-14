f378cce43a9ffdb63243d577e9f8377c5cd5b67c
genexusbot <builder@genexus.com>
2023-07-14 16:29:42 -0300

    Package version: 1.0.0-preview.20230714192150

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXEOAlipayInvoke",
+	platforms: [.iOS("12.0")],
+	products: [
+		.library(
+			name: "GXEOAlipayInvoke",
+			targets: ["GXEOAlipayInvokeWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "1.0.0-preview.20230714192150")
+	],
+	targets: [
+		.target(name: "GXEOAlipayInvokeWrapper",
+				dependencies: [
+					"GXEOAlipayInvoke",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
+					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.iOS])),
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
+					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXEOAlipayInvoke",
+			url: "https://pkgs.genexus.dev/iOS/preview/GXEOAlipayInvoke-1.0.0-preview.20230714192150.xcframework.zip",
+			checksum: "d4a20ac5b850d32e9c960c683bf96f901587567fb23a0b121f60e2b63cc80388"
+		)
+	]
+)
